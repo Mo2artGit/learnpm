ba624fbfdb1d0b24593b2c80780014a0bac7735b
Raymond Chen <97559049+Mo2artGit@users.noreply.github.com>
2024-02-11 18:19:40 +0000

    Initial commit

--- src/components/Assessment.js
+++ src/components/Assessment.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import img from './img/img.jpg';
+import { Button } from 'react-bootstrap';
+
 
 function Assessment() {
   return (
@@ -7,13 +9,14 @@ function Assessment() {
       <h1>Assessment</h1>
       <img src={img} alt="img" style={{ width: '30%' }} />
       <p>This is the Assessment page.</p>
+      <Button variant="light" >Contact Us</Button>
       <div className="d-flex justify-content-start">
         <ul>
           <li>1</li>
           <li>2</li>
         </ul>
       </div>
-
+      <Button variant="light" >Contact Us</Button>
     </div>
   );
 }
