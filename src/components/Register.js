ef260bb1975be1577847e4db167c64ace38df175
Raymond Chen <97559049+Mo2artGit@users.noreply.github.com>
2024-03-07 02:27:11 +0000

    register page update

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,121 +1,130 @@
 import React from 'react';
 import { Button, Form, Col, Row, Container } from 'react-bootstrap';
+import Footer from './Footer';
+import register from './img/register.svg';
+//import registerBg from './img/register_bg.png';
 
 function Register() {
+
+  var divStyle = {
+    minHeight: '75vh',
+
+  };
+
   return (
-    <Container className='text-start'>
-      <h2>
-        Welcome!
-      </h2>
-      <p>Register Below To Create Your Account</p>
-      <Form>
-        <Form.Group as={Row} className="mb-3" controlId="formName">
-          <Form.Label column sm={2}>
-            *Full Name:
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control type="text" />
-          </Col>
-        </Form.Group>
-
-        <Form.Group as={Row} className="mb-3" controlId="formEmail">
-          <Form.Label column sm={2}>
-            *Email
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control type="email" />
-          </Col>
-        </Form.Group>
-
-        <Form.Group as={Row} className="mb-3" controlId="formPassword">
-          <Form.Label column sm={2}>
-            *Password
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control type="password" />
-          </Col>
-        </Form.Group>
-
-        <Form.Group as={Row} className="mb-3" controlId="formCountry">
-          <Form.Label column sm={2}>
-            *Country:
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control type="text" />
-          </Col>
-        </Form.Group>
-
-        <Form.Group as={Row} className="mb-3" controlId="formMail">
-          <Col sm={2}>
-            <Form.Label>Mailing Address</Form.Label>
-          </Col>
-          <Col sm={10}>
-            <Form.Control as="textarea" rows={3} />
-          </Col>
-        </Form.Group>
-
-        <Form.Group as={Row} className="mb-3" controlId="formNumber">
-          <Form.Label column sm={2}>
-            Contact No. :
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control type="text" />
-          </Col>
-        </Form.Group>
-
-        <Form.Group as={Row} className="mb-3" controlId="formCourse">
-          <Form.Label column sm={2}>
-            Courses :
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Select aria-label="Courses">
-              <option disabled>Select Course</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </Form.Select>
-          </Col>
-        </Form.Group>
-
-        <Form.Group as={Row} className="mb-3" controlId="formPayment">
-          <Form.Label column sm={2}>
-            Payment Info:
-          </Form.Label>
-          <Col sm={10}>
-            <Row>
-              <Col>
-                <Form.Control type="text" placeholder="Cardholder Name" />
-              </Col>
-              <Col>
-                <Form.Control type="text" placeholder="Card Number" />
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Form.Control type="text" placeholder="MM/YY" />
-              </Col>
-              <Col>
-                <Form.Control placeholder="CVC" />
-              </Col>
-            </Row>
-          </Col>
-        </Form.Group>
-
-        <Row className='text-center'>
+    <div>
+      <div className='px-5 py-3' style={divStyle}>
+        <h2 className='fst-italic'>Welcome!</h2>
+        <h4 className='fst-italic'>Register Below To Create Your Account</h4>
+        <Row className='mt-4'>
           <Col>
-            <Button variant="primary" type="submit">
-              Reset
-            </Button>
+            <Container>
+              <img src={register} alt="register" />
+            </Container>
           </Col>
           <Col>
-            <Button variant="primary" type="submit">
-              Register
-            </Button>
+            <Container className='text-start'>
+              <Form>
+                <Form.Group className="mb-3" controlId="formName">
+                  <Form.Label>
+                    *Name:
+                  </Form.Label>
+                  <Form.Control type="text" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formEmail">
+                  <Form.Label>
+                    *Email:
+                  </Form.Label>
+                  <Form.Control type="email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formPassword">
+                  <Form.Label>
+                    *Password:
+                  </Form.Label>
+                  <Form.Control type="password" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formCountry">
+                  <Form.Label>
+                    *Country:
+                  </Form.Label>
+                  <Form.Control type="text" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formMail">
+                  <Form.Label>
+                    Address:
+                  </Form.Label>
+                  <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formNumber">
+                  <Form.Label>
+                    Contact No. :
+                  </Form.Label>
+                  <Form.Control type="text" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formCourse">
+                  <Form.Label>
+                    Courses:
+                  </Form.Label>
+
+                  <Form.Select aria-label="Courses">
+                    <option disabled>Select Course</option>
+                    <option value="1">Professional Scrum Master Training</option>
+                    <option value="2">CSM - Certified Scrum Master</option>
+                    <option value="3">CSPO - Certified Scrum Product Owner</option>
+                  </Form.Select>
+
+                </Form.Group>
+
+                {/*
+                <Form.Group className="mb-3" controlId="formPayment">
+                  <Form.Label>
+                    Payment Info:
+                  </Form.Label>
+                  <Row>
+                    <Col>
+                      <Form.Control type="text" placeholder="Cardholder Name" />
+                    </Col>
+                    <Col>
+                      <Form.Control type="text" placeholder="Card Number" />
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <Form.Control type="text" placeholder="MM/YY" />
+                    </Col>
+                    <Col>
+                      <Form.Control placeholder="CVC" />
+                    </Col>
+                  </Row>
+                </Form.Group>
+                */}
+
+                <Row className='text-center'>
+                  <Col>
+                    <Button variant="outline-secondary" className="w-100" type="submit">
+                      Reset
+                    </Button>
+                  </Col>
+                  <Col>
+                    <Button variant="secondary" className="w-100" type="submit">
+                      Register
+                    </Button>
+                  </Col>
+                </Row>
+
+              </Form>
+            </Container>
           </Col>
         </Row>
-
-      </Form>
-    </Container>
+      </div>
+      <Footer />
+    </div>
   );
 }
 
