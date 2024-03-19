eacb3bcea1806e066c504e43b00de0ccc1251903
Raymond Chen <97559049+Mo2artGit@users.noreply.github.com>
2024-03-19 18:58:55 +0000

    error_fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
-import Footer from './Footer'; // Make sure this Footer component matches your design
+import Footer from './Footer';
 import frontImage from './img/contactus_front_img.svg';
 import backgroundImage from './img/contactus_bg_img.png';
 
@@ -25,14 +25,14 @@ function Contact() {
 
     const pageStyle = {
         position: 'relative',
-        minHeight: '100vh',
+        minHeight: '75vh',
         display: 'flex',
         flexDirection: 'column',
         backgroundColor: 'rgba(255, 255, 255, 0.8)',
         overflow: 'hidden',
         zIndex: 0, // Ensure this is 0 or greater
     };
-    
+
     const backgroundStyle = {
         position: 'absolute',
         top: 0,
@@ -44,7 +44,7 @@ function Contact() {
         backgroundRepeat: 'no-repeat',
         opacity: 0.5,
         zIndex: -1, // Set to a negative value
-    };    
+    };
 
     const headerStyle = {
         padding: '20px 0',
@@ -75,71 +75,73 @@ function Contact() {
     };
 
     return (
-        <div style={pageStyle}>
-            <div style={{...backgroundStyle, backgroundImage: `url(${backgroundImage})`}}></div>
-            <h1 style={headerStyle}>Get In Touch! Contact our team below</h1>
-            <Container>
-                <Row style={formRowStyle}>
-                    <Col md={6} className="text-center">
-                        <img src={frontImage} alt="Contact Us" style={{ maxWidth: '100%', height: 'auto', borderRadius: '24px' }} />
-                    </Col>
-                    <Col md={6} style={formStyle}>
-                        <Form onSubmit={handleSubmit}>
-                            <Form.Group>
-                                <Form.Label style={requiredFieldLabelStyle}>Name*:</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Enter your name"
-                                    value={name}
-                                    onChange={(e) => setName(e.target.value)}
-                                />
-                            </Form.Group>
-                            <div style={{ marginBottom: '3px' }} />
-                            <Form.Group>
-                                <Form.Label style={labelStyle}>Email*:</Form.Label>
-                                <Form.Control
-                                    type="email"
-                                    placeholder="Enter your email"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                />
-                            </Form.Group>
-                            <div style={{ marginBottom: '3px' }} />
-                            <Form.Group>
-                                <Form.Label style={labelStyle}>Phone Number:</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Enter your phone number"
-                                    value={phone}
-                                    onChange={(e) => setPhone(e.target.value)}
-                                />
-                            </Form.Group>
-                            <div style={{ marginBottom: '3px' }} />
-                            <Form.Group>
-                                <Form.Label style={labelStyle}>Issue:</Form.Label>
-                                <Form.Control
-                                    as="textarea"
-                                    rows={3}
-                                    placeholder="What can we help you with?"
-                                    value={issue}
-                                    onChange={(e) => setIssue(e.target.value)}
-                                />
-                            </Form.Group>
-                            <div style={{ marginBottom: '20px' }} />
-                            <Button className="me-2" variant="secondary" type="reset">
-                                Reset
-                            </Button>
-                            <Button variant="primary" type="submit" className="text-center">
-                                Send
-                            </Button>
-                        </Form>
-                    </Col>
-                </Row>
-            </Container>
+        <>
+            <div style={pageStyle}>
+                <div style={{ ...backgroundStyle, backgroundImage: `url(${backgroundImage})` }}></div>
+                <h1 style={headerStyle}>Get In Touch! Contact our team below</h1>
+                <Container>
+                    <Row style={formRowStyle}>
+                        <Col md={6} className="text-center">
+                            <img src={frontImage} alt="Contact Us" style={{ maxWidth: '100%', height: 'auto', borderRadius: '24px' }} />
+                        </Col>
+                        <Col md={6} style={formStyle}>
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Group>
+                                    <Form.Label style={labelStyle}>Name*:</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Enter your name"
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                    />
+                                </Form.Group>
+                                <div style={{ marginBottom: '3px' }} />
+                                <Form.Group>
+                                    <Form.Label style={labelStyle}>Email*:</Form.Label>
+                                    <Form.Control
+                                        type="email"
+                                        placeholder="Enter your email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    />
+                                </Form.Group>
+                                <div style={{ marginBottom: '3px' }} />
+                                <Form.Group>
+                                    <Form.Label style={labelStyle}>Phone Number:</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Enter your phone number"
+                                        value={phone}
+                                        onChange={(e) => setPhone(e.target.value)}
+                                    />
+                                </Form.Group>
+                                <div style={{ marginBottom: '3px' }} />
+                                <Form.Group>
+                                    <Form.Label style={labelStyle}>Issue:</Form.Label>
+                                    <Form.Control
+                                        as="textarea"
+                                        rows={3}
+                                        placeholder="What can we help you with?"
+                                        value={issue}
+                                        onChange={(e) => setIssue(e.target.value)}
+                                    />
+                                </Form.Group>
+                                <div style={{ marginBottom: '20px' }} />
+                                <Button className="me-2" variant="secondary" type="reset">
+                                    Reset
+                                </Button>
+                                <Button variant="primary" type="submit" className="text-center">
+                                    Send
+                                </Button>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
             <Footer />
-        </div>
+        </>
     );
-    
+
 }
 
 export default Contact;
