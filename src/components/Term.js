99ae14957accf61a59cfdb28460c94ceb402f59a
Raymond Chen <97559049+Mo2artGit@users.noreply.github.com>
2024-04-03 18:24:53 +0000

    term page

--- src/components/Term.js
+++ src/components/Term.js
@@ -1,12 +1,77 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
-function Term() {
-  return (
-    <div>
-      <h1>Terms & conditions</h1>
-      <p>This is the Term page.</p>
-    </div>
-  );
-}
+const Term= () => {
+    const genTextStyle = {
+        width: '1596px',
+        height: '100%',
+        backgroundColor: 'rgba(255, 255, 255, 0)',
+        boxSizing: 'border-box',
+        fontFamily: '"Arial", sans-serif',
+        color: '#333333',
+        textAlign: 'left',
+        lineHeight: 'normal',
+    };
+
+    const headingStyle = {
+        fontWeight: 'bold', // this will make the headings bold
+    };
+
+    return (
+        <React.Fragment>
+            <Container style={genTextStyle}>
+                <h3 style={headingStyle}>Terms & Conditions</h3>
+                <p>These Terms and Conditions (“Terms”) govern your relationship with https://www.LearnPM.com/, products, features, services, websites, and applications (the “Service”) operated by LearnPM, Inc. (“LearnPM,” “us”, “we”, or “our”). Please read these Terms and Conditions carefully before using the Service. Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms and the Privacy Policy. These Terms apply to all visitors, users and others who access or use the Service. By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms, then you may not access the Service.</p>
+
+                <h3 style={headingStyle}>Purchases</h3>
+                <p>If you wish to purchase any product or service made available through the Service (“Purchase”), you may be asked to supply certain information relevant to your Purchase including, without limitation, your credit card number, the expiration date of your credit card, your billing address, and your shipping information. You represent and warrant that: (i) you have the legal right to use any credit card(s) or other payment method(s) in connection with any Purchase; and that (ii) the information you supply to us is true, correct and complete. The Service may employ the use of third-party services for the purpose of facilitating payment and the completion of Purchases. By submitting your information, you grant us the right to provide the information to these third-parties subject to our Privacy Policy. We reserve the right to refuse or cancel your order at any time for reasons including but not limited to: product or service availability, errors in the description or price of the product or service, error in your order or other reasons. We reserve the right to refuse or cancel your order if fraud or an unauthorized or illegal transaction is suspected.</p>
+
+                <h3 style={headingStyle}>Availability, Errors & Inaccuracies</h3>
+                <p>We are constantly updating product and service offerings on the Service. We may experience delays in updating information on the Service and in our advertising on other websites. The information found on the Service may contain errors or inaccuracies and may not be complete or current. Products or services may be mispriced, described inaccurately, or unavailable on the Service and we cannot guarantee the accuracy or completeness of any information found on the Service. We therefore reserve the right to change or update information and to correct errors, inaccuracies, or omissions at any time without prior notice.</p>
+
+                <h3 style={headingStyle}>Subscriptions</h3>
+                <p>Subscription Billing Some parts of the Service are billed on a subscription basis (“Subscription(s)”). You will be billed in advance on a recurring and periodic basis (“Billing Cycle”). Billing cycles are set either on a monthly or annual basis, depending on the type of subscription plan you select when purchasing a Subscription. At the end of each Billing Cycle, your Subscription will automatically renew under the exact same conditions unless you cancel it or LearnPM cancels it. You may cancel your Subscription renewal either through your online account management page or by contacting LearnPM customer support team. A valid payment method, including credit card, PayPal, or purchase order is required to process the payment for your Subscription. You shall provide LearnPM with accurate and complete billing information including full name, address, state, zip code, telephone number, and a valid payment method information. By submitting such payment information, you automatically authorize LearnPM to charge all Subscription fees incurred through your account to any such payment instruments. Should automatic billing fail to occur for any reason, LearnPM will issue an electronic invoice indicating that you must proceed manually, within a certain deadline date, with the full payment corresponding to the billing period as indicated on the invoice. Free Trial LearnPM may, at its sole discretion, offer a Subscription with a free trial for a limited period of time (“Free Trial”). You may be required to enter your billing information in order to sign up for the Free Trial. If you do enter your billing information when signing up for the Free Trial, you will not be charged by LearnPM until the Free Trial has expired. On the last day of the Free Trial period, unless you cancelled your Subscription, you will be automatically charged the applicable Subscription fees for the type of Subscription you have selected. At any time and without notice, LearnPM reserves the right to (i) modify the terms and conditions of the Free Trial offer, or (ii) cancel such Free Trial offer. Fee Changes LearnPM in its sole discretion and at any time, may modify the Subscription fees for the Subscriptions. Any Subscription fee change will become effective at the end of the then-current Billing Cycle.   LearnPM will provide you with a reasonable prior notice of any change in Subscription fees to give you an opportunity to terminate your Subscription before such change becomes effective. Your continued use of the Service after the Subscription fee change comes into effect constitutes your agreement to pay the modified Subscription fee amount. Refunds Certain refund requests for Subscriptions may be considered by LearnPM on a case-by-case basis and granted in sole discretion of LearnPM.</p>
+                
+                <h3 style={headingStyle}>LearnPM</h3>
+                <p>Some parts of the Service are billed on a course(s) registration basis.</p>
+               
+                
+                <h3 style={headingStyle}>Seasonal Training Promotion Terms & Conditions: NEWYEAR (active)</h3>
+                <p>1. The discount code is applicable to eligible public training courses and cannot be used for shipping, fees, taxes, or any other additional charges unless specifically stated. <br/>2. The discount code is valid through March 31, 2024. <br/>3. The discount code can only be used for classes delivered on or before March 31, 2024.<br/>4. The discount code cannot be retroactively applied to previous purchases. It can only be used for future orders placed during the specified valid period. <br/>5. The discount code cannot be used for the following courses: <br/>&emsp;• American Management Association® <br/>&emsp;• Training AWS Official Training<br/>&emsp;• Certified Agile Leader (CAL) Training<br/>&emsp;• Certified Scrum Professional® Training<br/>&emsp;• Certified SAFe® Release Train Engineer (RTE)<br/>&emsp;• Implementing SAFe® with Certified SAFe® Practice Consultant (SPC)<br/>&emsp;• ISTQB® Certified Tester Training<br/>&emsp;• Microsoft Training<br/>&emsp;• PMI™ Training<br/>  &emsp;  • Scrum@Scale Training<br/>&emsp;• Scrum.org™ Training<br/>For terms specifically related to purchases of LearnPM products and services, please view here: https://www.LearnPM.com/</p>
+
+                <h3 style={headingStyle}>Accounts</h3>
+                <p>Children under the age of 18 are not permitted to use our Service. You must be 18 or older to use our Service. When you create an account with us, you must provide us information that is accurate, complete, and current at all times. Failure to do so constitutes a breach of the Terms, which may result in immediate termination of your account on our Service. You are responsible for safeguarding the password that you use to access the Service and for any activities or actions under your password, whether your password is with our Service or a third-party service. You agree not to disclose your password to any third-party. You must notify us immediately upon becoming aware of any breach of security or unauthorized use of your account. You may not use as a username, the name of another person, or entity or that is not lawfully available for use, a name or trademark that is subject to any rights of another person or entity other than you, without appropriate authorization. We reserve the right to refuse service, terminate accounts, remove or edit content, or cancel orders in our sole discretion.</p>
+
+                <h3 style={headingStyle}> Intellectual Property </h3>
+                <p>The Service and its original content, features and functionality are and will remain the exclusive property of LearnPM. The Service is protected by copyright, trademark, and other laws of both the United States and foreign countries. Our trademarks and trade dress may not be used in connection with any product or service without the prior written consent of LearnPM.</p>
+
+                <h3 style={headingStyle}>Links to other websites</h3>
+                <p>Our Service may contain links to third-party websites or services that are not owned or controlled by LearnPM. LearnPM has no control over, and assumes no responsibility for the content, privacy policies, or practices of any third-party websites or services. We do not warrant the offerings of any of these entities/individuals or their websites. You acknowledge and agree that LearnPM shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such third-party websites or services. We strongly advise you to read the terms and conditions and privacy policies of any third-party websites or services that you visit.</p>
+
+                <h3 style={headingStyle}>Termination</h3>
+                <p>We may terminate or suspend your account and bar access to the Service immediately, without prior notice or liability, under our sole discretion, for any reason whatsoever and without limitation, including but not limited to a breach of the Terms. If you wish to terminate your account, you may simply discontinue using the Service or please contact us through email, wansh99@bu.edu. All provisions of the Terms which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.</p>
+
+                <h3 style={headingStyle}>Indemnification</h3>
+                <p>You agree to defend, indemnify and hold harmless LearnPM and their employees, contractors, agents, officers and directors, from and against any and all claims, damages, obligations, losses, liabilities, costs or debt, and expenses (including but not limited to attorney’s fees), resulting from or arising out of a) your use and access of the Service, by you or any person using your account and password, or b) a breach of these Terms.</p>
+
+                <h3 style={headingStyle}>Limitation of liability</h3>
+                <p>In no event shall LearnPM nor its directors, employees, partners, agents, suppliers, or affiliates, be liable for any indirect, incidental, special, consequential or punitive damages, including without limitation, loss of profits, data, use, goodwill, or other intangible losses, resulting from (i) your access to or use of or inability to access or use the Service; (ii) any conduct or content of any third-party on the Service; (iii) any content obtained from the Service; and (iv) unauthorized access, use or alteration of your transmissions or content, whether based on warranty, contract, tort (including negligence) or any other legal theory, whether or not we have been informed of the possibility of such damage, and even if a remedy set forth herein is found to have failed of its essential purpose.</p>
+
+                <h3 style={headingStyle}>Disclaimer</h3>
+                <p>Your use of the Service is at your sole risk. The Service is provided on an “AS IS” and “AS AVAILABLE” basis. The Service is provided without warranties of any kind, whether express or implied, including, but not limited to, implied warranties of merchantability, fitness for a particular purpose, non-infringement or course of performance. LearnPM and its subsidiaries do not warrant that: (i) the Service will function uninterrupted, secure or available at any particular time or location; (ii) any errors or defects will be corrected; (iii) the Service is free of viruses or other harmful components; or (iv) the results of using the Service will meet your requirements. Exclusions Some jurisdictions do not allow the exclusion of certain warranties or the exclusion or limitation of liability for consequential or incidental damages, so the limitations above may not apply to you.</p>
+
+                <h3 style={headingStyle}>Governing Law</h3>
+                <p>These Terms shall be governed and construed in accordance with the laws of California, United States, without regard to its conflict of law provisions. Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service and supersede and replace any prior agreements we might have had between us regarding the Service.</p>
+
+                <h3 style={headingStyle}>Changes</h3>
+                <p>We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material, we will provide at least thirty (30) days’ notice prior to any new terms taking effect by doing one or more of the following: (i) posting the changes through the Service; or (ii) sending you an email message about the changes. What constitutes a material change will be determined at our sole discretion. By continuing to access or use our Service after those revisions become effective, you agree to be bound by the revised terms. If you do not agree to the new terms, please stop using the Service.</p>
+
+                <h3 style={headingStyle}>Contact</h3>
+                <p>If you have any questions about these Terms, please contact us through email, wangsh99@bu.edu</p>
+                
+            </Container>
+        </React.Fragment>
+    );
+};
 
 export default Term;
