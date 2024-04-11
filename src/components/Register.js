cfb59992572f6dd30bae6af468963fb1e6a73b61
Raymond Chen <97559049+Mo2artGit@users.noreply.github.com>
2024-04-11 17:39:07 +0000

    contact and register page validation

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,23 +1,27 @@
 import React, { useState } from 'react';
 import { Button, Form, Col, Row, Container, Modal } from 'react-bootstrap';
 import register from './img/register.svg';
-//import registerBg from './img/register_bg.png';
 
 function Register() {
-
   const [showModal, setShowModal] = useState(false);
+  const [validated, setValidated] = useState(false);
+  const [formSubmitted, setFormSubmitted] = useState(false);
 
   const handleCloseModal = () => setShowModal(false);
   const handleShowModal = () => setShowModal(true);
-
   const handleSubmit = (event) => {
+    const form = event.currentTarget;
     event.preventDefault();
-    handleShowModal();
+    setValidated(true);
+
+    if (form.checkValidity() === true) {
+      handleShowModal();
+      setFormSubmitted(true);
+    }
   };
 
   var divStyle = {
     minHeight: '75vh',
-
   };
 
   return (
@@ -33,33 +37,45 @@ function Register() {
           </Col>
           <Col>
             <Container className='text-start'>
-              <Form>
+              <Form noValidate validated={validated} onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formName">
                   <Form.Label>
                     *Name:
                   </Form.Label>
-                  <Form.Control type="text" />
+                  <Form.Control type="text" required />
+                  <Form.Control.Feedback type="invalid">
+                    Please enter your name.
+                  </Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formEmail">
                   <Form.Label>
                     *Email:
                   </Form.Label>
-                  <Form.Control type="email" />
+                  <Form.Control type="email" required />
+                  <Form.Control.Feedback type="invalid">
+                    Please enter a valid email address.
+                  </Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formPassword">
                   <Form.Label>
                     *Password:
                   </Form.Label>
-                  <Form.Control type="password" />
+                  <Form.Control type="password" required />
+                  <Form.Control.Feedback type="invalid">
+                    Please enter a password.
+                  </Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formCountry">
                   <Form.Label>
                     *Country:
                   </Form.Label>
-                  <Form.Control type="text" />
+                  <Form.Control type="text" required />
+                  <Form.Control.Feedback type="invalid">
+                    Please enter your country.
+                  </Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formMail">
@@ -80,48 +96,22 @@ function Register() {
                   <Form.Label>
                     Courses:
                   </Form.Label>
-
                   <Form.Select aria-label="Courses">
                     <option disabled>Select Course</option>
                     <option value="1">Professional Scrum Master Training</option>
                     <option value="2">CSM - Certified Scrum Master</option>
                     <option value="3">CSPO - Certified Scrum Product Owner</option>
                   </Form.Select>
-
                 </Form.Group>
-
-                {/*
-                <Form.Group className="mb-3" controlId="formPayment">
-                  <Form.Label>
-                    Payment Info:
-                  </Form.Label>
-                  <Row>
-                    <Col>
-                      <Form.Control type="text" placeholder="Cardholder Name" />
-                    </Col>
-                    <Col>
-                      <Form.Control type="text" placeholder="Card Number" />
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col>
-                      <Form.Control type="text" placeholder="MM/YY" />
-                    </Col>
-                    <Col>
-                      <Form.Control placeholder="CVC" />
-                    </Col>
-                  </Row>
-                </Form.Group>
-                */}
 
                 <Row className='text-center'>
                   <Col>
-                    <Button variant="outline-secondary" className="w-100" type="submit">
+                    <Button variant="outline-secondary" className="w-100" type="reset">
                       Reset
                     </Button>
                   </Col>
                   <Col>
-                    <Button variant="secondary" className="w-100" type="submit" onClick={handleSubmit}>
+                    <Button variant="secondary" className="w-100" type="submit">
                       Register
                     </Button>
                   </Col>
@@ -131,19 +121,21 @@ function Register() {
           </Col>
         </Row>
       </div>
-      <Modal show={showModal} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Register Form</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>Registered!</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseModal}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      {formSubmitted && (
+        <Modal show={showModal} onHide={handleCloseModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Registration Successful</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Thank you for registering!</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseModal}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )}
     </div>
   );
 }
